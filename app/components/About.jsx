382f4da82fcdcd39c47c9bfd789a00f7bef37204
SRIHARI SARAVANAN <SRIHARI SARAVANAN>
2017-01-31 13:15:20 +0530

    pushingtoheroku

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,8 @@ var React=require('react');
 
 var About=(props)=> {
     return(
-      <h2>About component</h2>
+      <h2>About</h2>
+      <h4>Done by Hari.S</h4>
     );
 }
 module.exports=About;
