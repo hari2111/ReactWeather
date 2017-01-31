ecca57b6466c0b2b5a8e358a864255d72ab46907
SRIHARI SARAVANAN <SRIHARI SARAVANAN>
2017-01-31 13:29:04 +0530

    update2

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,10 @@ var React=require('react');
 
 var About=(props)=> {
     return(
+      <div>
       <h2>About</h2>
       <h4>Done by SRIHari.S.S</h4>
+      </div>
     );
 }
 module.exports=About;
